8e0ad98f363ffed43538a90b577d5d60addf2f1a
willian <viniciussousaitb35@gmail.com>
2026-09-11 16:59:17 +0100

    first commit

---
+++ src/main/java/com/dev/java/notificacaoPedidos/NotificacaoPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.dev.java.notificacaoPedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacaoPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacaoPedidosApplication.class, args);
+	}
+
+}
